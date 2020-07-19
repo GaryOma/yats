64e118ce45fe0251e02fb25e5f0c2cdc4a79b833
Gary SUBLET <gary.sublet@hotmail.fr>
2020-07-19 19:50:45 +0200

    add and update arguments

--- yats/__main__.py
+++ yats/__main__.py
@@ -7,20 +7,23 @@ from yats.connector import Connector
 
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--username",
-                        "-u",
-                        type=str,
-                        default="realdonaldtrump")
-    parser.add_argument("--thread",
-                        "-t",
-                        type=int,
-                        default=10)
+    parser = argparse.ArgumentParser(prog="yats", usage="%(prog)s [options]",
+                                     description="A fast Twitter scraper")
+    # parser.add_argument("--username", "-u", type=str,
+    #                     default="realdonaldtrump")
+    parser.add_argument("username", type=str, default="realdonaldtrump")
+    parser.add_argument("--thread", "-t", type=int, default=10)
+    parser.add_argument("--timeline", "-tl", action="store_true")
+    parser.add_argument("--output", "-o", type=str, default="/tmp/tweets.json")
+    con = Connector()
     args = parser.parse_args()
-    con = Connector(args.username)
-    profile = con.profile(args.username)
-    profile.describe()
-    con.get_tweets_user(args.username, thread_nb=args.thread)
+    if args.timeline:
+        tweets = con.get_tweets_timeline(args.username)
+    else:
+        tweets = con.get_tweets_user(args.username, thread_nb=args.thread)
+    tweets.export(args.output)
+    # profile = con.profile(args.username)
+    # profile.describe()
 
 
 if __name__ == "__main__":

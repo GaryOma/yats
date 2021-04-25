a305d4b0d4dcd2735337c2516db11de1cf6ca98b
Gary SUBLET <gary.sublet@hotmail.fr>
2021-04-25 12:11:16 +0200

    wip

--- yats/request.py
+++ yats/request.py
@@ -10,41 +10,44 @@ DEFAULT_TIMEOUT = 15
 
 class Request:
 
-    def __init__(self, proxy=None):
+    def __init__(self, proxy=None, use_https=True):
+        self.use_https = use_https
         self.proxy = proxy
 
     def _parse_content_type(self, c_type):
         res = re.match(r"(?P<type>\w+?)\/(?P<app>\w+?);"
                        r"\s*charset=(?P<charset>.+)",
                        c_type)
+        print(c_type)
         return res.groupdict()
 
     def _parse_body(self, raw, content_type):
         if content_type["app"] == "json":
             body = json.loads(raw)
         else:
+            # print(raw)
             body = raw.decode(content_type["charset"])
         return body
 
     def _send(self, type, host, path, headers):
         logging.debug("will send" + host)
-        if not hasattr(self, "https"):
+        if not hasattr(self, "connection"):
             self.recreate_connection(host, reset_proxy=True)
         if self.proxy is not None:
             # self.https.set_tunnel(host)
             # self.recreate_connection(host)
             logging.debug("setting tunnel")
             try:
-                self.https.set_tunnel(host, port=443, headers=headers)
+                self.connection.set_tunnel(host, port=443, headers=headers)
             except RuntimeError:
                 logging.error("Runtime error, refreshing")
                 self.recreate_connection(host)
-                self.https.set_tunnel(host, port=443, headers=headers)
+                self.connection.set_tunnel(host, port=443, headers=headers)
                 logging.info("Runtime error, validated")
             logging.debug("tunnel set")
             # self.https.set_tunnel(host, port=443)
         else:
-            self.https.host = host
+            self.connection.host = host
         self.body = None
         logging.debug("entering the loop")
         while self.body is None:
@@ -52,9 +55,9 @@ class Request:
             try:
                 if self.proxy is not None:
                     # self.https.request(type, path)
-                    self.https.request(type, path, headers=headers)
+                    self.connection.request(type, path, headers=headers)
                 else:
-                    self.https.request(type, path, headers=headers)
+                    self.connection.request(type, path, headers=headers)
             except http.client.CannotSendRequest:
                 logging.error("Cannot send request, refreshing connection")
                 # self.recreate_connection(host)
@@ -76,7 +79,7 @@ class Request:
                 return False
             # parse the response
             try:
-                response = self.https.getresponse()
+                response = self.connection.getresponse()
             except socket.timeout:
                 logging.error("timeout while reading response, refreshing")
                 # self.recreate_connection(host)
@@ -90,19 +93,22 @@ class Request:
             )
             self.response = response
             self.headers = response.getheaders()
+            self.body2 = response.read()
+            print(self.headers)
             try:
                 self.body = self._parse_body(response.read(), content_type)
+                print(self.body)
             except socket.timeout:
                 # self.recreate_connection(host)
                 logging.error("timeout while reading body, refreshing")
                 continue
         logging.debug("body sucessfully read")
-        self.https.close()
+        self.connection.close()
         return True
 
     def recreate_connection(self, host, reset_proxy=False):
-        if hasattr(self, "https"):
-            self.https.close()
+        if hasattr(self, "connection"):
+            self.connection.close()
         if self.proxy is not None:
             if reset_proxy:
                 logging.info("resetting the proxy")
@@ -114,18 +120,29 @@ class Request:
             else:
                 proxy = self.current_proxy
             logging.debug(f"create new connection with proxy {proxy[0]}")
-            self.https = http.client.HTTPSConnection(
-                proxy[0],
-                proxy[1],
-                timeout=DEFAULT_TIMEOUT)
+            if self.use_https:
+                self.connection = http.client.HTTPSConnection(
+                    proxy[0],
+                    proxy[1],
+                    timeout=DEFAULT_TIMEOUT)
+            else:
+                self.connection = http.client.HTTPConnection(
+                    proxy[0],
+                    proxy[1],
+                    timeout=DEFAULT_TIMEOUT)
             logging.info("connection resetted")
             # self.https.set_debuglevel(1000)
         else:
             url_parsed = urllib.parse.urlparse(host)
-            logging.debug(f"create new connection with {url_parsed.netloc}")
-            self.https = http.client.HTTPSConnection(
-                url_parsed.path,
-                timeout=DEFAULT_TIMEOUT)
+            logging.debug(f"create new connection with {url_parsed.path}")
+            if self.use_https:
+                self.connection = http.client.HTTPSConnection(
+                    url_parsed.path,
+                    timeout=DEFAULT_TIMEOUT)
+            else:
+                self.connection = http.client.HTTPConnection(
+                    url_parsed.path,
+                    timeout=DEFAULT_TIMEOUT)
 
     def get(self, url, params=None, headers={}):
         url_parsed = urllib.parse.urlparse(url)
@@ -133,6 +150,7 @@ class Request:
         if params is not None:
             payload = urllib.parse.urlencode(params)
             path += f"?{payload}"
+        print(path, headers, url_parsed.netloc)
         ret = self._send("GET", url_parsed.netloc, path, headers)
         logging.debug(f"return from the send {ret}")
         return ret
